b3fcf66434f136be2fd6240a082214faff9ad9fa
AnNaTe07 <102766959+AnNaTe07@users.noreply.github.com>
2022-06-17 12:01:30 -0300

    borrado comentarios por defecto

--- src/transversal_grupo4/Transversal_grupo4.java
+++ src/transversal_grupo4/Transversal_grupo4.java
@@ -1,22 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package transversal_grupo4;
 
 import Data.Conexion;
 import java.sql.Connection;
 
-/**
- *
- * @author Usuario
- */
 public class Transversal_grupo4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        Conexion conec = new Conexion();
        Connection x = conec.getConexion();
